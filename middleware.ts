1d3c724d6463eadbeea78dbf386fdc9cc4b659a9
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2024-11-04 01:33:24 +0530

    feat(middleware): enhance authentication and route protection
    - Integrated `createRouteMatcher` to define public routes for unauthenticated access.
    - Implemented role-based access control for authenticated users:
      - Admin users are redirected to the admin dashboard when accessing the dashboard.
      - Non-admin users are prevented from accessing admin routes and redirected to the general dashboard.
      - Authenticated users are restricted from accessing public routes, redirecting them based on their role.
    - Added error handling for user data fetching from Clerk, redirecting to an error page on failure.
    - Updated route matcher configuration to streamline middleware application.
    
    This update improves security and user experience by ensuring proper access control across the application.

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,72 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, clerkClient, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
-export default clerkMiddleware()
+const isPublicRoute = createRouteMatcher(
+    [
+        "/",
+        "/api/webhook/register",
+        "/sign-up",
+        "/sign-in",
+    ]
+)
+
+
+export default clerkMiddleware(
+    async (auth, request) => {
+        const { userId } = await auth();
+
+        //NOTE - secure protected routes from users without any authentication, redirects to sign-in page
+        if (!userId && !isPublicRoute(request)) {
+            return NextResponse.redirect(new URL('/sign-in', request.url))
+
+        }
+
+        //NOTE - filter designated role driven users to access desired routes
+        if (userId) {
+            try {
+                const client = await clerkClient()
+
+                const user = await client.users?.getUser(userId)
+
+                const role = user?.publicMetadata?.role as string | undefined
+
+                //NOTE - admin user access to admin dashboard
+                if (role === "admin" && request.url.includes("/dashboard")) {
+                    return NextResponse.redirect(new URL('/admin/dashboard', request.url))
+
+                }
+
+                //NOTE - prevent autherized non-admin users from accessing admin dashboard
+                if (role !== "admin" && request.url.match("/admin")) {
+                    return NextResponse.redirect(new URL('/dashboard', request.url))
+
+                }
+
+                //Note - restrict authenticated users from accessing public routes
+                if (isPublicRoute(request)) {
+                    return NextResponse.redirect(
+                        new URL(
+                            role === "admin" ? "/admin/dashboard" : "/dashboard",
+                            request.url
+                        )
+                    )
+
+                }
+            } catch (error: any) {
+                console.error("Error fetching user data from Clerk:", error);
+
+                return NextResponse.redirect(new URL('/error', request.url))
+
+            }
+
+        }
+
+
+    }
+)
 
 export const config = {
     matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
+        "/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"
     ],
 }
